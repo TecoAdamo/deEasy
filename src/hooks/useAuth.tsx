2d8aa7c5d12a982a07af4f22f5704c82feb99b4f
TecoAdamo <adamomateus478@gmail.com>
2024-05-17 15:50:18 -0300

    refactor: files and components that will no longer be used

--- src/hooks/useAuth.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from 'react'
-
-import { AuthContext } from '../context/CartContext'
-
-export function useAuth() {
-    const context = useContext(AuthContext)
-    return context
-}
