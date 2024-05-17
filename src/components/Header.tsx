ff1e821f6e4f130b4c5bf856f959c28fc629e694
TecoAdamo <adamomateus478@gmail.com>
2024-05-17 15:47:11 -0300

    refactor: sharing data with shopping cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,19 +4,26 @@ import { useNavigation } from '@react-navigation/native'
 import { AuthNavigatorRoutesProps } from '../routes/AuthRoutes'
 
 import Ionicons from '@expo/vector-icons/Ionicons'
-import { AuthContext } from '../context/CartContext'
+
+import React from 'react'
+import { useCartStore } from '../stores/cart-store'
 
 type Props = {
     title: string
+    cartQuantityItems: number
 }
 
 export default function Header({ title }: Props) {
     const navigation = useNavigation<AuthNavigatorRoutesProps>()
+    const cartQuantityItems = useCartStore((state) => state.products.length)
 
     const handleGoBack = () => {
         navigation.goBack()
     }
 
+    const handleGoCart = () => {
+        navigation.navigate('cart')
+    }
     return (
         <View style={styles.header}>
             <TouchableOpacity onPress={handleGoBack}>
@@ -24,11 +31,11 @@ export default function Header({ title }: Props) {
             </TouchableOpacity>
             <Text style={styles.textHeader}>{title}</Text>
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={handleGoCart}>
                 <View style={styles.headerBag}>
                     <Ionicons name="bag-outline" size={26} color="white" />
                     <View style={styles.badge}>
-                        <Text style={styles.textBag}>0</Text>
+                        <Text style={styles.textBag}>{cartQuantityItems}</Text>
                     </View>
                 </View>
             </TouchableOpacity>
