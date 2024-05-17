18ce84cb90f879feb5bb761c7b0f412fbbaeabb5
TecoAdamo <adamomateus478@gmail.com>
2024-05-16 23:00:05 -0300

    feat: button of sale

---
+++ src/components/BuyButton.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+type Props = {
+    title: string
+}
+
+export default function BuyButton({ title }: Props) {
+    return (
+        <TouchableOpacity style={styles.buttonBtn}>
+            <Text style={styles.buttonText}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonBtn: {
+        padding: 10,
+        width: '80%',
+        borderRadius: 10,
+        marginTop: '20%',
+        marginLeft: '10%',
+        backgroundColor: '#DF2613',
+    },
+    buttonText: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: '700',
+        textAlign: 'center',
+    },
+})
