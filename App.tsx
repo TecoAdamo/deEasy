10440092418b967e21bc50e419adcb96d930a2f3
TecoAdamo <adamomateus478@gmail.com>
2024-05-20 21:35:18 -0300

    refactor: NativeBaseProvider Added

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Routes } from './src/routes'
 
 import { StatusBar } from 'react-native'
-
 import { NativeBaseProvider } from 'native-base'
 
 export default function App() {
